59af1b467115ee630b1ee1212791d6ab493e68d9
dennisdang5 <dennisdang5@gmail.com>
2026-08-07 16:38:32 -0700

    Created config settings

--- earnings_rag/config.py
+++ earnings_rag/config.py
@@ -5,4 +5,23 @@ class Settings(BaseSettings):
 
     db_host: str = 'localhost'
     db_port: int = 5432
-    db_
+    db_name: str = 'earnings'
+    db_user: str = 'postgres'
+    db_password: str = 'postgres'
+
+    sec_user_agent: str = ''
+    embedding_api_key: str = ''
+    llm_api_key: str = ''
+
+    chunk_size_tokens: int = 500
+    chunk_overlap_tokens: int = 50
+    top_k: int = 5
+
+    @property
+    def db_url(self) -> str:
+        return (
+            f'postgresql://{self.db_user}:{self.db_password}'
+            f'@{self.db_host}:{self.db_port}/{self.db_name}'
+        )
+
+settings = Settings()
